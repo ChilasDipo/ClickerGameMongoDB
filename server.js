7085ea21cfc49ed16b1a75dd64ad1f797cd5ea23
ChilasDipo <Chil0041@edu.easj.dk>
2021-11-07 20:59:04 +0100

    Can save and load from database, need more work

--- server.js
+++ server.js
@@ -1,78 +1,140 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const foodRouter = require("./routes/foodRoutes");
 const indexRouter = require('./routes/index');
 const javaRouter = require('./routes/JSRoute');
 const cssRouter = require('./routes/CSSRouter');
-
-//const saveOnServerRoute = require('./routes/saveOnServer')
+const ObjectId = require('mongodb').ObjectId;
 
 const app = express();
-app.use(express.urlencoded({ extended: true}))
 app.use(express.json());
 
+let money = 0
+let level = 1
+let priceForLevel=10
 
 const { MongoClient } = require('mongodb');
 const { collection } = require("./models/food");
-const uri = "mongodb+srv://admin:Password@cluster0.vp0pp.mongodb.net/ClickerGame?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("Connected" + collection.dbName)
-  
+const url = "mongodb+srv://admin:Password@cluster0.vp0pp.mongodb.net/ClickerGame?retryWrites=true&w=majority";
+const client = new MongoClient(url);
 
+ // The database to use
 
-  
-  client.close();
-});
+ const dbName = "users";                 
 
-var money = 0
-var level = 1
-var priceForLevel=10
+ async function run(name) {
+    try {
+         await client.connect();
+         console.log("Connected correctly to server");
+         const db = client.db(dbName);
+         const col = db.collection("data");
+         const myDoc = await col.findOne({"name" : name});
+        money = myDoc.data[0]
+        level = myDoc.data[1]
+        priceForLevel = myDoc.data[2]
+         console.log(myDoc);
+         return myDoc
+        } catch (err) {
+         console.log(err.stack);
+     }
+     finally {
+      
+        await client.close();
+       
+    }
 
+}
+//let name = "user1"
 
+//run()
 
-app.post('/clicked',express.json({type: '*/*'}), (req,res) => {
-  console.log(res.json(req.body))
-  console.log(req.body)
-  console.log(req.body)
-  console.log("Clicked")
-  res.send({
-    "Folk": [{
-      "name": "Gustav",
-      "age": "old",
-      "height": "Tall"
-    }, {
-      "name": "Chilas",
-      "age": "Less Old",
-      "height": "Less Tall"
-    },{
-      "name": "TallBoy",
-      "age": "Not Old",
-      "height": "Tallest"
-    }]
-  })
-    
- // let arrayForData = [money+parseInt(req.body),level,priceForLevel]
-  //let arrayJSON = JSON.stringify(arrayForData)
-  //console.log(arrayJSON)
+app.post('/loadUser', (req,res) => {
+console.log(req.body)
+console.log(req.body.user.name)
+run(req.body.user.name)
+  arrayJSON = JSON.stringify({money,level,priceForLevel}) 
+   console.log("then")
+  res.send(arrayJSON)  
+
+ 
+})
+
+app.post('/clicked', (req,res) => {
+  money = money + 1
+  arrayJSON = JSON.stringify({money,level,priceForLevel})
+  res.send(arrayJSON)   
+
 })
 app.post('/levelUp', (req,res) => {
   if(money>priceForLevel){
     level = level + 1
     money= money-priceForLevel
     priceForLevel= priceForLevel + 2
+    arrayJSON = JSON.stringify({money,level,priceForLevel})
+    res.send(arrayJSON)  
 }
 })
-app.get('/data', (req,res) => {
-  
-  res.send(arrayJSON)
+app.post('/reset', (req,res) => {
+  money = 0
+  level = 1
+  priceForLevel = 10
+  arrayJSON = JSON.stringify({money,level,priceForLevel})
+  res.send(arrayJSON)  
 })
+
+app.post('/save', (req,res) => {
+save(req.body.user.name)
+})
+
+//setInterval(() => {
+ // save()
+//}, 20000);
+
+async function save(username){  
+  try {
+
+    await client.connect();
+
+    console.log("Connected correctly to server");
+
+    const db = client.db(dbName);
+
+    // Use the collection "data"
+
+    const col = db.collection("data");
+
+    // Construct a document                                                                                                                                                              
+
+    let savedata = {
+        "name": username ,                                                                                                                               
+        "data": [ money, level, priceForLevel ],
+    }
+
+    // Insert a single document, wait for promise so we can read it back
+
+    //const p = await col.insertOne(savedata);
+
+    // Find one document
+    await col.replaceOne({"name" : username},savedata);
+    
+    // set values to thoses from database
+
+
+   } catch (err) {
+
+    console.log(err.stack);
+
+}
+
+
+
+finally {
+   await client.close();
+}
+
+}
 
 
 app.use(indexRouter)
-app.use(foodRouter);
 app.use(javaRouter)
 app.use(cssRouter)
 
--- views/javascript.js
+++ views/javascript.js
@@ -1,148 +1,64 @@
+const userName = "temp"
+console.log(document.cookie)
 
-var money = 0
-var level = 1
-var priceForLevel=10
-console.log("Works")
-console.log(money)
-
-let amountOfClicksInTheLast5Sec = 0
-
-function yesclick(){
-    amountOfClicksInTheLast5Sec = amountOfClicksInTheLast5Sec + 1
-    console.log("works")
-
-    var payload = 2
-    
-    var data = new FormData();
-    data.append( "json",  JSON.stringify(payload)  );
-    console.log(JSON.stringify(payload) )
-    fetch("/clicked",
+async function checkUser(){
+if (document.cookie != ""){
+ userName = document.cookie
+}else{
+    console.log("start")
+  //  let respone = await fetch("/reset",{  method: "POST",})
+  //  let result =  await respone.json()
+  //  updateUI(result)
+}
+}
+async function selectUser(){
+let respone = await fetch("/loadUser",
     {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'},
-        body : {
-            "Folk": [{
-                "name": "Gustav",
-                "age": "old",
-                "height": "Tall"
-            }, {
-                "name": "Chilas",
-                "age": "Less Old",
-                "height": "Less Tall"
-            },{
-                "name": "TallBoy",
-                "age": "Not Old",
-                "height": "Tallest"
-            }]
-        }
-
-    })
-    .then(function(res){ return res.json(); })
-    .then(function(data){ alert( JSON.stringify( data ) ) })
-
-//updateUI()
-}
-
-
-
-// setInterval(
-//     function(){
-//         let data = new URLSearchParams();
-//         data.append(`key`, `value`);
-//         data.append(`anotherKey`, `another value`);
-
-//         const options = {
-//             method: `POST`,
-//             body: data
-//           };
-
-
-//         fetch('/clicked',options)
-//         amountOfClicksInTheLast5Sec = 0; 
-//     },5000
-// )
-
-
-function plusLevel(){
-    fetch('/levelUp',{method: 'POST'}) 
-updateUI()
-}
-
-
-function updateUI(){
-
+        method: "post",
+        headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ "user": {
+      "name" : document.getElementById("nameField").value,
+    }})
     
-
-
-    //document.getElementById('money').innerHTML= "du har " + money + " £"
-    //document.getElementById('level').innerHTML= "du er level" + level
-    //document.getElementById('priceForlevel').innerHTML = "Prisen for at levelup er " + priceForLevel
+})
+let result = await respone.json()
+    updateUI(result)
+}
+ async function clicked(){
+    console.log("works")
+    let respone = await fetch("/clicked",{  method: "POST",})
+    let result =  await respone.json()
+    updateUI(result)
+}
+async function plusLevel(){
+   let respone = await fetch("/levelUp",{  method: "POST",})
+   let result =  await respone.json()
+   updateUI(result)
+}
+async function updateUI(result){
+    document.getElementById('money').innerHTML= "du har " + result.money + " £"
+    document.getElementById('level').innerHTML= "du er level" + result.level
+    document.getElementById('priceForlevel').innerHTML = "Prisen for at levelup er " + result.priceForLevel
 }
 
-function saveWithLocalStorage(){
-    localStorage.setItem("money", money)
-    localStorage.setItem("level", level)
-    localStorage.setItem("priceForLevel", priceForLevel)
-    
+async function start(){
+    checkUser()
 }
 
-function saveWithCoockie(){
-document.cookie = +money 
-}
-
-function saveWithSession(){
-    sessionStorage.setItem("money", money)
-    sessionStorage.setItem("level", level)
-    sessionStorage.setItem("priceForLevel", priceForLevel)
-    
-}
-
-function loadWithCoockie(){
-    money = parseInt(document.cookie)
-}
-function loadlWithLocalStorage(){
-    if(localStorage.getItem("money")!=null){
-      money = parseInt(localStorage.getItem("money"))  
-    }
-    
-    if(localStorage.getItem("level")!=null){
-        level = parseInt(localStorage.getItem("level"))  
-      }
-priceForLevel = parseInt(localStorage.getItem("priceForLevel"))
-}
-
-function loadlWithSessionStorage(){
-    if(sessionStorage.getItem("money")!=null){
-      money = parseInt(sessionStorage.getItem("money"))  
-    }
-    
-    if(sessionStorage.getItem("level")!=null){
-        level = parseInt(sessionStorage.getItem("level"))  
-      }
-      if(sessionStorage.getItem("priceForLevel")!=null){
-        priceForLevel = parseInt(sessionStorage.getItem("priceForLevel"))  
-      }
-
-}
-
-function reset(){
-    money = 0
-    level = 1
-    priceForLevel = 10
-    updateUI()
-}
-
-function save(){
-    saveWithSession()
-}
-
-function start(){
-    loadlWithSessionStorage()
-    updateUI()
+async function reset(){
+    let respone = await fetch("/reset",{  method: "POST",})
+    let result =  await respone.json()
+    updateUI(result)
 }
 
 function saveOnServer(){
-    fetch('/clicked',{method: 'POST'}) 
+    fetch("/save",
+    {
+        method: "post",
+        headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({ "user": {
+      "name" : document.getElementById("nameField").value,
+    }})
+})
 }
 
